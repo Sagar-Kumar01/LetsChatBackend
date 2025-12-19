bd889e927ea99c0b5543c5480ba4128518ab5fbe
= <sagarsoni32896@gmail.com>
2025-12-19 19:27:31 +0530

    edit server.js

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './configs/db.js';
-import {inngest,functions} from './inngest/index.js';
-import { serve } from 'inngest/express';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { serve } from "inngest/express";
+
+import connectDB from "./configs/db.js";
+import { inngest, functions } from "./inngest/index.js";
+
+dotenv.config();
 
 const app = express();
-dotenv.config();
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-
-await connectDB();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+ await connectDB();
+
+/**
+ * Health check
+ */
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 const PORT = process.env.PORT || 5000;
